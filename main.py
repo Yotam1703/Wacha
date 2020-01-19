aac119c15496d1a466706348063227a46df18d92
Yotam1703 <42112977+Yotam1703@users.noreply.github.com>
2020-01-19 12:12:03 +0200

    Update main.py
    Changed system. no longer checks running average, but instead on ever increasing parts of the corpus.

--- main.py
+++ main.py
@@ -5,12 +5,12 @@
 
 def WrdPerf(word,isStart,isEnd): 
                 CritFilled = 0
-                if isStart: ##word is in head of sentence
-                                CritFilled += 1
-                # if isEnd:
+                #if isStart: ##word is in head of sentence
+                #                CritFilled += 1
+                #if isEnd:
                 #                 CritFilled += 1
-                # if len(word) <= 4: 
-                #                 CritFilled += 1
+                if len(word) <= 4: 
+                                  CritFilled += 1
                 # if len(word) >= 6: 
                 #                 CritFilled -= 1              
                 # if word.endswith("ים"): 
@@ -18,7 +18,7 @@ def WrdPerf(word,isStart,isEnd):
                 # if word.endswith("ות"): 
                 #                 CritFilled += 1
                 # if word[0] == "ה":
-                #               CritFilled -= 1 
+                #             CritFilled -= 1 
                 # if word[0] == "ב":
                 #               CritFilled -= 1
                 # if word[0] == "מ":
@@ -31,7 +31,18 @@ def WrdPerf(word,isStart,isEnd):
                 #               CritFilled -= 1
                 # if word[0] == "ש":
                 #               CritFilled += 1
-
+                #if len(word) == 2: 
+                #                CritFilled += 1
+                #if len(word) == 3: 
+                #                CritFilled += 1
+                #if len(word) == 4: 
+                #                CritFilled += 1
+                #if len(word) == 5: 
+                #                CritFilled += 1                
+                # if word[0] == "ע":
+                #               CritFilled += 1  
+                
+                
                 IsAmb = CritFilled >= 1 ##One criterion filled is enough (???)
                 return IsAmb
 
@@ -99,7 +110,7 @@ Sentence21 = "אין לנו צורך באנשים ללא כל ידע שינסו 
 RealSentencePerf21 = [True,True,True,False,False,False,True,True,False,True,False]
 Sentence22 = "אלו אנשים שידם קלה על המקלדת אנשים שלא חושבים פעמיים מי נמצא בצד השני"
 RealSentencePerf22 = [True,True,True,True,True,False,False,False,False,False,True,False,False,True]
-Sentence23 = "כותבת הפוסט הישראלית שמספרת כי אביה הוא בעל משק חקלאי כבר שלושים שנים ביקשה בפוסט שאם חשובה לכם מדינת ישראל והחקלאות הישראלית התעלמו מהסרטון תסביאו לחברים שלכם מה זה אומר ותזכירו לכולם שבישראל רק חקלאות ישראלית"
+Sentence23 = "כותבת הפוסט הישראלית שמספרת כי אביה הוא בעל משק חקלאי כבר שלושים שנים ביקשה בפוסט שאם חשובה לכם מדינת ישראל והחקלאות הישראלית התעלמו מהסרטון תסבירו לחברים שלכם מה זה אומר ותזכירו לכולם שבישראל רק חקלאות ישראלית"
 RealSentencePerf23 = [True,False,True,False,False,False,False,False,True,True,True,False,False,False,True,False,False,True,False,False,True,False,False,False,False,False,False,False,False,True,True,False,False,True,False,True]
 Sentence24 = "ותמשיכו לאכול חסה לא מסרטנת מלאה בברזל טובה לעיכול ועוד מלא סגולות ששמורות רק לה ולא לאף ירק אחר"
 RealSentencePerf24 = [False,True,True,False,False,True,False,True,False,False,True,True,True,True,False,False,True,True,True]
@@ -114,38 +125,15 @@ Sentence28 = "הסחר שהיקפו עומד על יותר מחמש מאות ט�
 RealSentencePerf28 = [False,False,True,False,True,True,False,False,True,False,False,True,True,False,False,True,True,True,False,True,True,True,True,False,True]
 
 
-def avg(numbers):
-    return sum(numbers)/len(numbers)
+BigSentence = [Sentence2,Sentence3,Sentence4,Sentence5,Sentence6,Sentence7,Sentence8,Sentence9,Sentence10,Sentence11,Sentence12,Sentence13,Sentence14,Sentence15,Sentence16,Sentence17,Sentence18,Sentence19,Sentence20,Sentence21,Sentence22,Sentence23,Sentence24,Sentence25,Sentence26,Sentence27,Sentence28]
+BigRealSentencePerf = [RealSentencePerf2,RealSentencePerf3,RealSentencePerf4,RealSentencePerf5,RealSentencePerf6,RealSentencePerf7,RealSentencePerf8,RealSentencePerf9,RealSentencePerf10,RealSentencePerf11,RealSentencePerf12,RealSentencePerf13,RealSentencePerf14,RealSentencePerf15,RealSentencePerf16,RealSentencePerf17,RealSentencePerf18,RealSentencePerf19,RealSentencePerf20,RealSentencePerf21,RealSentencePerf22,RealSentencePerf23,RealSentencePerf24,RealSentencePerf25,RealSentencePerf26,RealSentencePerf27,RealSentencePerf28]
+CutSentenceCorpus = Sentence1
+CutRealSentencePerfCorpus = RealSentencePerf1
 
-Eval1 = Eval(Sentence1, RealSentencePerf1)
-Eval2 = Eval(Sentence2, RealSentencePerf2)
-Eval3 = Eval(Sentence3, RealSentencePerf3)
-Eval4 = Eval(Sentence4, RealSentencePerf4)
-Eval5 = Eval(Sentence5, RealSentencePerf5)
-Eval6 = Eval(Sentence6, RealSentencePerf6)
-Eval7 = Eval(Sentence7, RealSentencePerf7)
-Eval8 = Eval(Sentence8, RealSentencePerf8)
-Eval9 = Eval(Sentence9, RealSentencePerf9)
-Eval10 = Eval(Sentence10, RealSentencePerf10)
-Eval11 = Eval(Sentence11, RealSentencePerf11)
-Eval12 = Eval(Sentence12, RealSentencePerf12)
-Eval13 = Eval(Sentence13, RealSentencePerf13)
-Eval14 = Eval(Sentence14, RealSentencePerf14)
-Eval15 = Eval(Sentence15, RealSentencePerf15)
-Eval16 = Eval(Sentence16, RealSentencePerf16)
-Eval17 = Eval(Sentence17, RealSentencePerf17)
-Eval18 = Eval(Sentence18, RealSentencePerf18)
-Eval19 = Eval(Sentence19, RealSentencePerf19)
-Eval20 = Eval(Sentence20, RealSentencePerf20)
-Eval21 = Eval(Sentence21, RealSentencePerf21)
-Eval22 = Eval(Sentence22, RealSentencePerf22)
-Eval23 = Eval(Sentence23, RealSentencePerf23)
-Eval24 = Eval(Sentence24, RealSentencePerf24)
-Eval25 = Eval(Sentence25, RealSentencePerf25)
-Eval26 = Eval(Sentence26, RealSentencePerf26)
-Eval27 = Eval(Sentence27, RealSentencePerf27)
-Eval28 = Eval(Sentence28, RealSentencePerf28)
-Eval = [Eval1,Eval2,Eval3,Eval4,Eval5,Eval6,Eval7,Eval8,Eval9,Eval10,Eval11,Eval12,Eval13,Eval14,Eval15,Eval16,Eval17,Eval18,Eval19,Eval20,Eval21,Eval22,Eval23,Eval24,Eval25,Eval26,Eval27,Eval28]
-
-
-print (avg(Eval))
+print(Eval(CutSentenceCorpus, CutRealSentencePerfCorpus))
+for i in range(27):
+    CutSentenceCorpus += " " + BigSentence[i]
+    CutRealSentencePerfCorpus += BigRealSentencePerf[i]
+    # print (CutSentenceCorpus)
+    print(Eval(CutSentenceCorpus, CutRealSentencePerfCorpus))
+# print (CutSentenceCorpus.split(" "))
